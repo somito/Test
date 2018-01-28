6214b94433bee9297d7eafadd4abf0b54b5f6eb8
Soma <somodian@hotmail.com>
2018-01-28 17:19:42 +0100

    separated tasks for the JsonParser class

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,9 +7,24 @@ namespace Test
         static void Main(string[] args)
         {
             AppStart.InitDataBasePath();
-            LinkGenerator linkGen = new LinkGenerator(2017, 350, 2);
+            LinkGenerator linkGen = new LinkGenerator(2017, 360, 2);
             Context db = new Context();
-            JsonParser.ParseScoringPlays(db, linkGen);
+
+            int failedCount = 0;
+            foreach (string link in linkGen)
+            {
+                JsonParser parser = new JsonParser(link);
+                if (parser.PageNotFound)
+                {
+                    failedCount += 1;
+                    if (failedCount == 2)
+                    {
+                        break;
+                    }
+                    continue;
+                }
+                JsonParser.ParseScoringPlays(db);
+            }
             db.SaveChanges();
             db.Dispose();
         }
